b589d01f36ac57f647bac1f71b0b82b6fba94daf
kodohee <aheneoaqu@gmail.com>
2024-03-04 20:36:27 +0900

    날씨 undefined 해결해야 됨

--- javascript-version/src/views/dashboard/Weather.js
+++ javascript-version/src/views/dashboard/Weather.js
@@ -16,8 +16,141 @@ import DotsVertical from 'mdi-material-ui/DotsVertical'
 import CurrencyUsd from 'mdi-material-ui/WeatherHurricane'
 import WeatherRainy from 'mdi-material-ui/WeatherRainy'
 
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+// env
+//REACT_APP_API_KEY = "3928ebf09d1e4495380386b521a3a0f1"
+// 불러오기
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const Weather = () => {
+  const [weather, setWeather] = useState({});
+  const [cityName, setCityName] = useState('');
+
+  const salesData = [
+    {
+      stats: `${weather.temp}°C`,
+      title: '기온',
+      color: 'primary',
+      icon: <TemperatureCelsius sx={{ fontSize: '1.75rem' }} />,
+    },
+    {
+      stats: `${weather.humidity}%`,
+      title: '습도',
+      color: 'success',
+      icon: <WeatherRainy sx={{ fontSize: '1.75rem' }} />,
+    },
+    {
+      stats: `${weather.windSpeed}`,
+      color: 'warning',
+      title: '바람',
+      icon: <WeatherWindy sx={{ fontSize: '1.75rem' }} />,
+    },
+    {
+      stats: `${weather.clouds}%`,
+      color: 'info',
+      title: '구름',
+      icon: <WeatherCloudy sx={{ fontSize: '1.75rem' }} />,
+    },
+  ];
+
+  const getWeather = async (lat, lon) => {
+    try {
+      const res = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+      );
+
+      const weatherId = res.data.weather[0].id;
+      const weatherIcon = res.data.weather[0].icon;
+      const weatherIconAdrs = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+      const temp = Math.round(res.data.main.temp);
+
+      const weatherKo = res.data.weather[0].main; // or whatever you want to assign to weatherKo
+      const cityName = res.data.name;
+
+      setWeather({
+        description: weatherKo,
+        name: cityName,
+        temp: temp,
+        icon: weatherIconAdrs,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      getWeather(lat, lon);
+    });
+  }, []);
+
+  const renderStats = () => {
+    return salesData.map((item, index) => (
+      <Grid item xs={12} sm={3} key={index}>
+        <Box key={index} sx={{ display: 'flex', alignItems: 'center' }}>
+          <Avatar
+            variant='rounded'
+            sx={{
+              mr: 3,
+              width: 44,
+              height: 44,
+              boxShadow: 3,
+              color: 'common.white',
+              backgroundColor: `${item.color}.main`,
+            }}
+          >
+            {item.icon}
+          </Avatar>
+          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Typography variant='caption'>{item.title}</Typography>
+            <Typography variant='h6'>{item.stats}</Typography>
+          </Box>
+        </Box>
+      </Grid>
+    ));
+  };
+
+  return (
+    <Card>
+      <CardHeader
+        title='오늘의 날씨'
+        action={
+          <IconButton size='small' aria-label='settings' className='card-more-options' sx={{ color: 'text.secondary' }}>
+            <DotsVertical />
+          </IconButton>
+        }
+        subheader={
+          <Typography variant='body2'>
+            <Box component='span' sx={{ fontWeight: 600, color: 'text.primary' }}>
+              흐린 후 맑음🌤
+            </Box>{' '}
+          </Typography>
+        }
+        titleTypographyProps={{
+          sx: {
+            mb: 2.5,
+            lineHeight: '2rem !important',
+            letterSpacing: '0.15px !important',
+          },
+        }}
+      />
+      <CardContent sx={{ pt: theme => `${theme.spacing(3)} !important` }}>
+        <Grid container spacing={[5, 0]}>
+          {renderStats()}
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Weather;
 
 
+/*
 
 const salesData = [
   {
@@ -45,6 +178,8 @@ const salesData = [
     icon: <WeatherCloudy sx={{ fontSize: '1.75rem' }} />
   }
 ]
+
+
 
 const renderStats = () => {
   return salesData.map((item, index) => (
@@ -87,7 +222,7 @@ const StatisticsCard = () => {
             <Box component='span' sx={{ fontWeight: 600, color: 'text.primary' }}>
               흐린 후 맑음🌤
             </Box>{' '}
-            
+          
           </Typography>
         }
         titleTypographyProps={{
@@ -108,3 +243,5 @@ const StatisticsCard = () => {
 }
 
 export default StatisticsCard
+
+*/
